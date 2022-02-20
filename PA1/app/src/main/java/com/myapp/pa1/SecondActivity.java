e70ef1d50cfc1f8b88e1dde113087a571e8b57b7
Bill <trantriducs@gmail.com>
2022-02-20 15:34:59 -0500

    feat: remove redundant import

--- PA1/app/src/main/java/com/myapp/pa1/SecondActivity.java
+++ PA1/app/src/main/java/com/myapp/pa1/SecondActivity.java
@@ -2,7 +2,6 @@ package com.myapp.pa1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
