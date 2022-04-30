3aca50c9046157cb21a06ad1ccfdb8724930a2ab
Julian Nguyen <pnguyen4@conncoll.edu>
2022-04-30 16:36:49 -0400

    feat: Adapter & viewHolder for Stock

---
+++ StockMarketTracker/app/src/main/java/com/myapp/stockmarkettracker/ui/StockAdapter.java
@@ -0,0 +1,63 @@
+package com.myapp.stockmarkettracker.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import com.myapp.stockmarkettracker.Stock;
+
+public class StockAdapter extends RecyclerView.Adapter<StockViewHolder> {
+
+    private ArrayList<Stock> stockList;
+    private OnItemClickedListener onItemClickedListener;
+
+    public StockAdapter(ArrayList<Stock> stockList, OnItemClickedListener onItemClickedListener) {
+        this.stockList = stockList;
+        this.onItemClickedListener = onItemClickedListener;
+    }
+
+    /**
+     *  Creating a Holder/Container View for each stock
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public StockViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_stock, parent, false);
+        return new StockViewHolder(view, onItemClickedListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return stockList == null ? 0 : stockList.size();
+    }
+
+    /**
+     * The info to be displayed on each row of stock
+     *
+     * @param stockViewHolder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull StockViewHolder stockViewHolder, int position) {
+        Stock stock = stockList.get(position);
+
+        // Display stock title
+        stockViewHolder.setStockName(stock.getCompanyName());
+    }
+
+    /**
+     *  Interface for listener when clicking each Stock item
+     */
+    public interface OnItemClickedListener{
+        void onStockClick(int position);
+    }
+}
---
+++ StockMarketTracker/app/src/main/java/com/myapp/stockmarkettracker/ui/StockViewHolder.java
@@ -0,0 +1,41 @@
+package com.myapp.stockmarkettracker.ui;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.myapp.stockmarkettracker.ui.StockAdapter.OnItemClickedListener;
+
+public class StockViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    private TextView stockNameTextView;
+    private TextView stockSymbolTextView;
+
+    OnItemClickedListener onItemClickedListener;
+
+    public StockViewHolder(@NonNull View itemView, OnItemClickedListener onItemClickedListener) {
+        super(itemView);
+
+        this.stockNameTextView = itemView.findViewById(R.id.stock_name);
+        this.stockSymbolTextView = itemView.findViewById(R.id.stock_symbol);
+
+        this.onItemClickedListener = onItemClickedListener;
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setStockName(String stockName) {
+        this.stockNameTextView.setText(stockName);
+    }
+
+    public void setStockSymbol(String stockSymbol) {
+        this.stockSymbolTextView.setText(stockSymbol);
+    }
+
+    @Override
+    public void onClick(View view) {
+        onItemClickedListener.onStockClick(getAdapterPosition());
+    }
+}
